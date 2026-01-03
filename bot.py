3c9794f19cb39d41efe5cab5bc49c7e7c54b57da
fazliddinjumanov <164179926+fazliddinjumanov@users.noreply.github.com>
2026-01-03 23:27:01 +0500

    Change bot status message from Koyeb to Railway

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = os.getenv("8436252310:AAH5fVrD5FLMBqodFbxP27q-9cUEjV6jZ6g")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot Koyeb'da ishlayapti ✅")
+    await update.message.reply_text("Bot Railway'da ishlayapti ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
